7ff70145ffca08633413bd34ecd28f2bc2143708
Zarahhhh <zakariyyahrahmat@gmail.com>
2024-07-05 08:38:21 -0700

    print base 16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -12,20 +12,20 @@ int main(void)
 {
 	int num;
 	int letter;
-	
+
 	/* Print the numbers 0 to 9 */
 	for (num = '0'; num <= '9'; num++)
 	{
 		putchar(num);
 	}
-	
+
 	/* Print the letters a to f */
 	for (letter = 'a'; letter <= 'f'; letter++)
 	{
 		putchar(letter);
 	}
-	
+
 	putchar('\n'); /* Print new line */
-	
+
 	return (0);
 }
