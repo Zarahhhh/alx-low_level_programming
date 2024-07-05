1aff2445911b0f08ee06d9837dd902f12d61273f
Zarahhhh <zakariyyahrahmat@gmail.com>
2024-07-05 08:31:48 -0700

    print basee 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+	int num;
+	int letter;
+	
+	/* Print the numbers 0 to 9 */
+	for (num = '0'; num <= '9'; num++)
+	{
+		putchar(num);
+	}
+	
+	/* Print the letters a to f */
+	for (letter = 'a'; letter <= 'f'; letter++)
+	{
+		putchar(letter);
+	}
+	
+	putchar('\n'); /* Print new line */
+	
+	return (0);
+}
